44419aa6be7285768717a07d04b67371daa7c886
Jonas Krödel <jonas.kroedel@gmail.com>
2023-02-14 22:26:08 +0100

    Initial Commit

---
+++ Wetter/WetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetterApp.swift
+//  Wetter
+//
+//  Created by Jonas Krödel on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct WetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
